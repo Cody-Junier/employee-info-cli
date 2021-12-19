25b5351396f78938034f1e39d7774e09a950cda2
cody-junier <codyjunier@gmail.com>
2021-12-18 19:19:10 -0800

    Further styling

--- src/employeeInfoTemplate.js
+++ src/employeeInfoTemplate.js
@@ -1,30 +1,36 @@
 const managerTemplate= (manager) =>{
     return `<div class="card" style="width: 18rem;">
-    <div class="card-body">
-      <h5 class="card-title">Manager ${manager.name}</h5>
-      <p class="card-text">ID: ${manager.id}</p>
-      <p class="card-text">Email: <a href= "mailto: ${manager.email}">${manager.email}</a></p>
-      <p class="card-text">Office Number: ${manager.office}</p>
-    </div>
+    <div class="card-body p-0">
+      <h5 class="card-title bg-danger p-2">Manager ${manager.name}</h5>
+      <ul class="card-text">
+        <li class="py-1">ID: ${manager.id}</li>
+        <li class="py-1">Email: <a href= "mailto: ${manager.email}">${manager.email}</a></li>
+        <li class="py-1">Office Number: ${manager.office}</li>
+        </ul>
+        </div>
   </div>`
 }
 const engineerTemplate= (engineer) =>{
     return `<div class="card" style="width: 18rem;">
-    <div class="card-body">
-      <h5 class="card-title">Engineer ${engineer.name}</h5>
-      <p class="card-text">ID: ${engineer.id}</p>
-      <p class="card-text">Email: <a href= "mailto: ${engineer.email}">${engineer.email}</a></p>
-      <p class="card-text">GitHub: <a href="https://github.com/${engineer.github}"target="_blank">${engineer.github}</a></p>
+    <div class="card-body p-0">
+      <h5 class="card-title bg-info p-2">Engineer ${engineer.name}</h5>
+      <ul class="card-text">
+      <li class="py-1">ID: ${engineer.id}</li>
+      <li class="py-1">Email: <a href= "mailto: ${engineer.email}">${engineer.email}</a></li>
+      <li class="py-1">GitHub: <a href="https://github.com/${engineer.github}"target="_blank">${engineer.github}</a></li>
+      </ul>
     </div>
   </div>`
 }
 const internTemplate= (intern) =>{
     return `<div class="card" style="width: 18rem;">
-    <div class="card-body">
-      <h5 class="card-title">Intern: ${intern.name}</h5>
-      <p class="card-text">ID: ${intern.id}</p>
-      <p class="card-text">Email: <a href= "mailto: ${intern.email}">${intern.email}</a></p>
-      <p class="card-text">School: ${intern.school}</p>
+    <div class="card-body p-0">
+      <h5 class="card-title bg-warning p-2">Intern: ${intern.name}</h5>
+      <ul class="card-text">
+      <li class="py-1">ID: ${intern.id}</li>
+      <li class="py-1">Email: <a href= "mailto: ${intern.email}">${intern.email}</a></li>
+      <li class="py-1">School: ${intern.school}</li>
+      </ul>
     </div>
   </div>`
 }
@@ -41,8 +47,10 @@ const genHtml =(workspace) =>{
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-
-    
+<header>
+<h1 class="text-center bg-black text-white p-5">Workspace Info</h1>
+</header>
+<main class="d-flex justify-content-center">
     ${workspace.filter(employee =>{
         if(employee.getRole()=== "Manager"){
             return employee
@@ -61,7 +69,7 @@ const genHtml =(workspace) =>{
         }}).map(intern =>{
             return internTemplate(intern)
         }).join("")}
-    
+</main>  
 </body>
 </html>`}
 
